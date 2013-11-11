f1d286c836b3f9eaa4efbf27824a719e1d0ae0b2
Nattakorn.Kingman@gmail.com <Nattakorn.Kingman@gmail.com@6271f13f-0cf3-29f2-bba8-8dcd25400b0e>
2013-11-11 07:18:08 +0000

    first project committed.

---
+++ EGCO380FirstStart/EGCO380FirstStart/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EGCO380FirstStart
+//
+//  Created by Guest User on 11/11/2556 BE.
+//  Copyright (c) 2556 Guest User. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ EGCO380FirstStart/EGCO380FirstStartTests/EGCO380FirstStartTests.m
@@ -0,0 +1,34 @@
+//
+//  EGCO380FirstStartTests.m
+//  EGCO380FirstStartTests
+//
+//  Created by Guest User on 11/11/2556 BE.
+//  Copyright (c) 2556 Guest User. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EGCO380FirstStartTests : XCTestCase
+
+@end
+
+@implementation EGCO380FirstStartTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
