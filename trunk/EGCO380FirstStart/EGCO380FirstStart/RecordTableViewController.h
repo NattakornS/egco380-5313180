2fe45279d0a5537a9131b0c35dc448f9aa419401
Nattakorn.Kingman@gmail.com <Nattakorn.Kingman@gmail.com@6271f13f-0cf3-29f2-bba8-8dcd25400b0e>
2013-11-12 08:44:08 +0000

    add tab view

---
+++ trunk/EGCO380FirstStart/EGCO380FirstStart/RecordTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecordTableViewController.h
+//  EGCO380FirstStart
+//
+//  Created by Forensic on 11/12/2556 BE.
+//  Copyright (c) 2556 Guest User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecordTableViewController : UITableViewController
+
+@end
